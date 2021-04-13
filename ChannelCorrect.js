ba64806a53a4fd5cd8f156c5b19fb2979854eee2
Archer2305 <73808514+Archer2305@users.noreply.github.com>
2021-04-13 13:18:11 -0700

    Create ChannelCorrect.js

---
+++ ChannelCorrect.js
@@ -0,0 +1,30 @@
+/**
+ * @name ChannelCorrecter
+ * @version 0.0.1
+ * @description Will detect if you are using a bot command and auto-redirect you to the bots channel
+ * @author Archer2305#7876
+ * @source
+ * 
+ */
+
+
+// var channels =document.getElementsByClassName("containerDefault--pIXnN")
+var channels =document.getElementsByClassName("containerDefault--pIXnN")
+var channelName =document.getElementsByClassName("containerDefault--pIXnN").textContent
+var PageName=document.getElementsByClassName("title-29uC1r")
+var channelHref = document.querySelectorAll("a[data-list-item-id]")
+for(var a=1;a<channels.length;a++){
+   if(channels[a].textContent.includes("bot")){
+     
+      var redirect=channelHref[a].href
+      var UserTyping= document.querySelector("span[data-slate-string]")
+      if(UserTyping.textContent.startsWith("pls") ||UserTyping.textContent.startsWith("!")||UserTyping.textContent.startsWith(".")||UserTyping.textContent.startsWith("/")||UserTyping.textContent.startsWith("\\")){
+         if(PageName[0].textContent!=channels[a].textContent){
+         BdApi.alert("Are You In The Right Channel?" , [ BdApi.React.createElement("a", {href: redirect}, "Redirect(restarts discord ")])
+         }
+      }
+  }   
+
+   }
+
+
